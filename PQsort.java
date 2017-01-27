dad83e53f1e8d656cee8babf6061451e246b17ad
S-Lyle <sarahkathrynlyle@gmail.com>
2017-01-27 16:25:38 -0700

    Update PQsort.java

--- PQsort.java
+++ PQsort.java
@@ -1,13 +1,10 @@
 /**
  * Created by Sarah on 1/24/2017.
  */
-public class PQsort implements Comparable<Collision> {
+public class PQsort  {
     public double compare(Collision one, Collision two){
         return two.getTime()-one.getTime();
-    }
+    } //if negative, one is farther away and two happens first.
+    //if positive, two is farther away and one happens first
 
-    @Override
-    public int compareTo(Collision o) {
-        return 0;
-    }
 }
