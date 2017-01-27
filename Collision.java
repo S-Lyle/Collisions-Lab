b665e930475d69784901fd1e950272a7ebd50381
S-Lyle <sarahkathrynlyle@gmail.com>
2017-01-27 16:24:04 -0700

    Update Collision.java

--- Collision.java
+++ Collision.java
@@ -1,16 +1,24 @@
 /**
  * Created by Sarah on 1/24/2017.
  */
-public class Collision {
+public class Collision implements Comparable<Collision> {
     double time_until;
-    Particle particle;
-    public Collision(double time, Particle a){
+    Particle particle1;
+    Particle particle2;
+    public Collision(double time, Particle a, Particle b){
         time_until=time;
-        particle=a;
+        particle1=a;
+        particle2=b;
     }
 
     public double getTime(){
         return time_until;
     }
+    public Particle getParticle1() {return particle1;}
+    public Particle getParticle2() {return particle2;}
 
+    @Override
+    public int compareTo(Collision o) {
+        return 0;
+    }
 }
