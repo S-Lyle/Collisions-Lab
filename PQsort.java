5ad7a064ce83391de2464f0f5c1ade255c15f8ea
S-Lyle <sarahkathrynlyle@gmail.com>
2017-02-03 17:19:37 -0700

    Update PQsort.java

--- PQsort.java
+++ PQsort.java
@@ -18,12 +18,12 @@ public class PQsort  {
         for (int i = 0; i < particles.length; i++) {
             double dt = a.timeToHit(particles[i]);
             if (clockTime + dt <= limit)
-                pq.insert(new Collision(clockTime + dt, a, particles[i]));
+                pq.insert(new Collision(dt, a, particles[i]));
         }
-        double dtX = a.collidesX();
+        double dtX = a.collidesX(); //time when a will collide with X or Y
         double dtY = a.collidesY();
-        if (clockTime + dtX <= limit) pq.insert(new Collision(clockTime + dtX, null, a));
-        if (clockTime + dtY <= limit) pq.insert(new Collision(clockTime + dtY, a, null));
+        if (clockTime + dtX <= limit) pq.insert(new Collision(dtX, null, a));
+        if (clockTime + dtY <= limit) pq.insert(new Collision(dtY, a,null));
     }
     public void redraw(double limit) {
         StdDraw.clear();
@@ -36,6 +36,17 @@ public class PQsort  {
             pq.insert(new Collision(((clockTime + 1.0) / .5), null, null));
         }*/
     }
+    public void move(Particle p, double dt){
+        double a =dt;
+        double inc=0.2;
+        while(inc<a){
+            p.changeX(p.getXi()+ p.getvX() * inc);
+            p.changeY(p.getYi()+p.getvY() * inc);
+            inc+=0.01;
+            redraw(1000);
+        }
+    }
+
 
     public void PQsorting(double timeLimit) {
         pq = new MinPQ<Collision>();
@@ -44,29 +55,34 @@ public class PQsort  {
         }
         pq.insert(new Collision(0, null, null));
 
+
         while (!pq.isEmpty()) {
 
-            // get impending event, discard if invalidated
             Collision main = pq.delMin();
             if (!main.isValid()) continue;
             Particle one = main.particle1;
             Particle two = main.particle2;
 
-            for (int i = 0; i < particles.length; i++) {
-                particles[i].move(main.time_until - clockTime);
+            //something is making the PQ code miss collisions, i think its how I implement time and the efficiency of this code
+            for (double inc = 0; inc <=main.time_until; inc+=0.01){
+                for (int i = 0; i < particles.length; i++) {
+                    particles[i].changeX(particles[i].getX() + particles[i].getvX() * 0.01); //make this match inc number
+                    particles[i].changeY(particles[i].getY() + particles[i].getvY() * 0.01);
+                }
+                redraw(1000);
             }
-            clockTime = main.time_until;
-
+            clockTime += main.time_until;
             if      (one != null && two != null) one.bounceOff(two);              // particle-particle collision
-            else if (one != null && two == null) one.bouncevY();   // particle-wall collision
-            else if (one == null && two != null) two.bouncevX(); // particle-wall collision
+            else if (one != null && two == null) one.bounceoffHorizontalWall();   // particle-wall collision
+            else if (one == null && two != null) two.bounceoffVerticalWall(); // particle-wall collision
             else if (one == null && two == null) redraw(timeLimit);
 
-            redraw(timeLimit);
 
             predict(one, timeLimit);
             predict(two, timeLimit);
 
+
         }
+
     }
 }
